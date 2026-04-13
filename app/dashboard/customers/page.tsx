26d23c87ac42c013c18df3a7842480a90799f379
aashishgulshan <aashish.gulshan@codiant.com>
2026-04-13 17:00:09 +0530

    refactor dashboard pages: enhance loading states, implement error handling, and improve data fetching logic

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,9 @@
-export default function Page() {
-  return <p>Customers Page</p>;
+import { Suspense } from "react";
+import CustomersTable from "@/app/ui/customers/table";
+export default async function Page() {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <CustomersTable />
+    </Suspense>
+  );
 }
--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,6 +1,6 @@
 // import "@/app/ui/globals.css";
-import SideNav from "../ui/dashboard/sidenav";
 import { inter } from "@/app/ui/fonts";
+import SideNav from "../ui/dashboard/sidenav";
 export default function Layout({children}: {children: React.ReactNode}) {
     return(
        <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
